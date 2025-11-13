8c6374e7c57719b973feb12d09f20547ff4ecd2d
podiukov.iv <ilia.podiukov@dom.ru>
2025-11-13 10:26:34 +0500

    upd files

--- flask_redis/README.md
+++
@@ -1,1 +0,0 @@
-# vparanoid8
--- flask_redis/app.py
+++ flask_redis/app.py
@@ -1,5 +1,6 @@
 import time
 import redis
+import socket
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,4 +12,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return 'Hello World version 5.0! I have been seen {} times. My name is: {}'.format(count, socket.gethostname())
